9ce76d4cd427c749ca33493f0c1d7aea080903f7
katrindreier <katrindreier@katrins-air>
2018-05-13 13:36:08 +0200

    test

--- Server.js
+++ Server.js
@@ -41,14 +41,14 @@ namespace Server {
     }
 }*/
 "use strict";
-var Http = require("http");
-var Url = require("url");
+const Http = require("http");
+const Url = require("url");
 var Server;
 (function (Server) {
-    var port = process.env.PORT;
+    let port = process.env.PORT;
     if (port == undefined)
         port = 8100;
-    var server = Http.createServer();
+    let server = Http.createServer();
     server.addListener("listening", handleListen);
     server.addListener("request", handleRequest);
     server.listen(port);
@@ -57,13 +57,11 @@ var Server;
     }
     function handleRequest(_request, _response) {
         console.log("Ich höre Stimmen!");
-        var query = Url.parse(_request.url, true).query;
-        var a = parseInt(query["a"]);
-        var b = parseInt(query["b"]);
-        for (var key in query)
+        let query = Url.parse(_request.url, true).query;
+        let a = parseInt(query["a"]);
+        let b = parseInt(query["b"]);
+        for (let key in query)
             console.log(query[key]);
-        a = 10;
-        b = 20;
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write("Ich habe dich gehört<br/>");
